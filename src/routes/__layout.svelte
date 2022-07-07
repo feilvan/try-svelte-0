5252ef323d0cd11072d282a4031a33a75e859410
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-07-07 22:27:51 +0700

    220707

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	window.addEventListener('resize', () => {
+		// We execute the same script as before
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	});
 </script>
 
 <svelte:head>
@@ -7,19 +12,30 @@
 </svelte:head>
 
 <main>
+	<header>
+		<div>machined</div>
+		<div>monster</div>
+	</header>
 	<slot />
 	<footer>
-		<span>©{new Date().getFullYear()} truclr</span>
+		<span>{new Date().getFullYear()}</span>
+		<span>work in progress...</span>
 	</footer>
 </main>
 
 <style>
 	main {
-		@apply flex flex-1 flex-col p-4 w-full h-screen max-w-5xl my-0 mx-auto;
+		@apply flex flex-1 flex-col p-4 w-full max-w-xl my-0 mx-auto;
+		height: 100vh;
+		height: calc(var(--vh, 1vh) * 100);
+	}
+
+	header {
+		@apply flex flex-row justify-between uppercase
 	}
 
 	footer {
-		@apply flex flex-col justify-center items-center;
+		@apply flex flex-row justify-between
 	}
 
 </style>
