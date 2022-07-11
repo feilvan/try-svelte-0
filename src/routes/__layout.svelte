caa7540686edb8df984296d90073071bbe915c5b
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-07-11 20:36:25 +0700

    220711

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,18 +11,27 @@
 </script>
 
 <svelte:head>
-  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">
+	<link
+		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 <main>
-	<header>
-		<div>machined</div>
-		<div>monster</div>
+	<header class="flex flex-row relative cornerTop">
+		<div class="flex flex-row relative justify-between uppercase border p-4 w-full cornerBottom">
+			<div>machined monster</div>
+			<div>in progress ...</div>
+		</div>
 	</header>
-	<slot />
+	<div class="flex flex-1 relative cornerTop my-4">
+		<div class="flex flex-1 relative cornerBottom border">
+			<slot />
+		</div>
+	</div>
 	<footer>
-		<span>{new Date().getFullYear()}</span>
-		<span>work in progress...</span>
+		<span>44exa</span>
+		<span>{new Date().getUTCFullYear()}</span>
 	</footer>
 </main>
 
@@ -33,12 +42,43 @@
 		height: calc(var(--vh, 1vh) * 100);
 	}
 
-	header {
-		@apply flex flex-row justify-between uppercase
-	}
-
 	footer {
-		@apply flex flex-row justify-between
+		@apply flex flex-row justify-between;
 	}
 
+	.cornerTop:before {
+		position: absolute;
+		left: 0px;
+		top: 0px;
+		content: '';
+		border-top: 20px solid white;
+		border-right: 20px solid transparent;
+	}
+
+	.cornerTop:after {
+		position: absolute;
+		left: -1px;
+		top: -1px;
+		content: '';
+		border-top: 20px solid black;
+		border-right: 20px solid transparent;
+	}
+
+	.cornerBottom:before {
+		position: absolute;
+		right: -1px;
+		bottom: -1px;
+		content: '';
+		border-bottom: 20px solid white;
+		border-left: 20px solid transparent;
+	}
+
+	.cornerBottom:after {
+		position: absolute;
+		right: -2px;
+		bottom: -2px;
+		content: '';
+		border-bottom: 20px solid black;
+		border-left: 20px solid transparent;
+	}
 </style>
